0c8c28446ea0034997d7be6f42a1146749b10433
Shubham2503 <46487179+Shubham2503@users.noreply.github.com>
2021-02-12 23:08:32 +0530

    added user page

--- client/src/pages/Users/index.js
+++ client/src/pages/Users/index.js
@@ -1,21 +1,45 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
-
+import Button from 'react-bootstrap/esm/Button'
+import styles from './index.module.css'
 const User = (props) => {
 
-    const [uid,setUid] = useState(null)
+    const [uid, setUid] = useState(null)
+    const [data, setData] = useState([])
 
-    useEffect(() => {
+
+    const getData = async () => {
+        const uri = '/user/' + uid
+        await axios.get(uri)
+            .then(data => {
+                setData(data.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    useEffect(async () => {
         setUid(props.match.params.uid)
-    }, [])
+        await getData()
+    }, [uid])
 
-    if(uid == null)
-    return null
+    if (uid == null || data === [])
+        return null
 
 
-    return(
-        <div>
-            <p>user </p>
-            <p>{uid}</p>
+    return (
+        <div className={styles.container}>
+            <h2>USER</h2>
+            <div className={styles.wraper}>
+                <p>First Name : {data.fname}</p>
+                <p>Last Name : {data.lname}</p>
+                <p>Email : {data.email}</p>
+                <p>Address : {data.address}</p>
+                {data.dob && <p>Date of birth : {data.dob.slice(0, 10)}</p>}
+                <p>Balance : {data.balance}</p>
+            </div>
+            <Button varient="primary">Transfer</Button>
         </div>
     )
 }
